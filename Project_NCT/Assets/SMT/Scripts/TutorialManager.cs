1567f7ad623c03c06174e1de5b0965b85009988a
Jaewan-Lim <42615916+JWdori@users.noreply.github.com>
2023-05-22 23:44:34 +0900

    [UPDATE] 일단 커밋

--- Project_NCT/Assets/SMT/Scripts/TutorialManager.cs
+++ Project_NCT/Assets/SMT/Scripts/TutorialManager.cs
@@ -3,18 +3,35 @@ using UnityEngine;
 public class TutorialManager : MonoBehaviour
 {
     public GameObject tutorialUI;
-
+    //public GameObject interactiveUIElements;
     private void Start()
     {
         // PlayerPrefs를 사용하여 튜토리얼을 한 번만 보여줌
         if (PlayerPrefs.GetInt("TutorialShown") == 0)
         {
             tutorialUI.SetActive(true);
+            //interactiveUIElements.SetActive(false);
             PlayerPrefs.SetInt("TutorialShown", 1);
+
+            // tutorialUI 외의 인터렉티브 UI 요소를 비활성화
+
+            // 시작 Z좌표를 -2로 설정
+            transform.position = new Vector3(transform.position.x, transform.position.y, -2f);
         }
         else
         {
             tutorialUI.SetActive(false);
+            // tutorialUI 외의 인터렉티브 UI 요소를 활성화
+
+            // 시작 Z좌표를 0으로 설정
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
         }
     }
+
+    public void SetStartPosition()
+    {
+        // 시작 Z좌표를 0으로 설정
+        transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
+    }
+
 }
