e3113a67e25c3371aed5be99e58d34cf5fd4333f
Jaewan-Lim <42615916+JWdori@users.noreply.github.com>
2023-05-23 17:55:34 +0900

    [update] 카메라 위치 변경

--- Project_NCT/Assets/SMT/Scripts/TutorialManager.cs
+++ Project_NCT/Assets/SMT/Scripts/TutorialManager.cs
@@ -4,6 +4,9 @@ public class TutorialManager : MonoBehaviour
 {
     public GameObject tutorialUI;
     //public GameObject interactiveUIElements;
+
+    public GameObject playerCamera; // 플레이어 카메라 오브젝트를 할당받을 변수
+
     private void Start()
     {
         // PlayerPrefs를 사용하여 튜토리얼을 한 번만 보여줌
@@ -12,26 +15,21 @@ public class TutorialManager : MonoBehaviour
             tutorialUI.SetActive(true);
             //interactiveUIElements.SetActive(false);
             PlayerPrefs.SetInt("TutorialShown", 1);
-
-            // tutorialUI 외의 인터렉티브 UI 요소를 비활성화
-
-            // 시작 Z좌표를 -2로 설정
-            transform.position = new Vector3(transform.position.x, transform.position.y, -2f);
+            Vector3 playerCameraPosition = playerCamera.transform.position;
+            playerCamera.transform.position = new Vector3(playerCameraPosition.x, playerCameraPosition.y, playerCameraPosition.z - 2f);
         }
         else
         {
             tutorialUI.SetActive(false);
             // tutorialUI 외의 인터렉티브 UI 요소를 활성화
-
-            // 시작 Z좌표를 0으로 설정
-            transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
         }
+
     }
 
     public void SetStartPosition()
     {
-        // 시작 Z좌표를 0으로 설정
-        transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
+        // 시작 Z좌표를 플레이어 카메라의 Z좌표로 설정
+        Vector3 playerCameraPosition = playerCamera.transform.position;
+        playerCamera.transform.position = new Vector3(playerCameraPosition.x, playerCameraPosition.y, 0);
     }
-
 }
