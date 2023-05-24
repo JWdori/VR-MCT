1875a847e5bc63e9f36fce17d27d28893fdee437
Jaewan-Lim <42615916+JWdori@users.noreply.github.com>
2023-05-25 02:10:08 +0900

    [update] 공이랑 바구니 구현, 풍선 파티클...

--- Project_NCT/Assets/SMT/Scripts/TutorialManager.cs
+++ Project_NCT/Assets/SMT/Scripts/TutorialManager.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using System.Collections;
 
 public class TutorialManager : MonoBehaviour
 {
     public GameObject tutorialUI;
+    public GameObject tutorialUI2;
+    public GameObject tutorialUIMessage;
+
     //public GameObject interactiveUIElements;
 
     public GameObject playerCamera; // 플레이어 카메라 오브젝트를 할당받을 변수
@@ -13,6 +17,9 @@ public class TutorialManager : MonoBehaviour
         if (PlayerPrefs.GetInt("TutorialShown") == 0)
         {
             tutorialUI.SetActive(true);
+            Vector3 playerCameraRotation = playerCamera.transform.rotation.eulerAngles;
+            playerCameraRotation.y = 180f;
+            playerCamera.transform.rotation = Quaternion.Euler(playerCameraRotation);
             //interactiveUIElements.SetActive(false);
             PlayerPrefs.SetInt("TutorialShown", 1);
             Vector3 playerCameraPosition = playerCamera.transform.position;
@@ -28,8 +35,22 @@ public class TutorialManager : MonoBehaviour
 
     public void SetStartPosition()
     {
+        tutorialUI2.SetActive(false);
+        tutorialUIMessage.SetActive(true);
+        StartCoroutine(DelayedStartPosition(2f)); // 1초 대기 후에 실행
+    }
+
+    private IEnumerator DelayedStartPosition(float delay)
+    {
+        yield return new WaitForSeconds(delay); // 지정한 시간(delay) 동안 대기
+
+        tutorialUI.SetActive(false);
         // 시작 Z좌표를 플레이어 카메라의 Z좌표로 설정
         Vector3 playerCameraPosition = playerCamera.transform.position;
-        playerCamera.transform.position = new Vector3(playerCameraPosition.x, playerCameraPosition.y, 0);
+        playerCamera.transform.position = new Vector3(playerCameraPosition.x, playerCameraPosition.y, 0f);
+        // Y 축 로테이션을 0으로 설정
+        Vector3 playerCameraRotation = playerCamera.transform.rotation.eulerAngles;
+        playerCameraRotation.y = 0f;
+        playerCamera.transform.rotation = Quaternion.Euler(playerCameraRotation);
     }
 }
