2f77c4f546dbd1cedb0c9e81fbb55971ea3c8fbb
Jaewan-Lim <42615916+JWdori@users.noreply.github.com>
2023-05-26 06:02:07 +0900

    [update] 풍선 및 효과 추가

---
+++ Project_NCT/Assets/SMT/Scripts/Balloon_Pop.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon_Pop : MonoBehaviour
+{
+    public ParticleSystem particleSystem;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        particleSystem.gameObject.SetActive(true);
+
+
+        ParticleSystem instantiatedParticleSystem = Instantiate(particleSystem, collision.contacts[0].point, Quaternion.identity);
+        instantiatedParticleSystem.Play();
+
+        // 충돌 위치에 파티클 시스템 작동
+
+        // 충돌한 오브젝트 파괴
+        Destroy(gameObject);
+    }
+}
---
+++ Project_NCT/Assets/SMT/Scripts/pop_audio.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pop_audio : MonoBehaviour
+{
+    public AudioClip audioClip; // 재생할 오디오 클립
+
+    public float spatialBlendMax = 1f; // spatialBlend의 최대치
+
+    private ParticleSystem particleSystem;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+        audioSource = gameObject.AddComponent<AudioSource>();
+        PlayAudio();
+    }
+
+    private void PlayAudio()
+    {
+        if (audioClip != null)
+        {
+            audioSource.clip = audioClip;
+            audioSource.spatialBlend = spatialBlendMax; // spatial blend 설정
+            audioSource.Play();
+        }
+    }
+
+    private void Update()
+    {
+        // 파티클 시스템이 재생 중이지 않으면 파괴
+        if (!particleSystem.IsAlive())
+        {
+            Destroy(gameObject);
+        }
+    }
+}
