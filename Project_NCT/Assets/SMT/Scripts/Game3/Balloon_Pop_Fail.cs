cd84ccf8b5efddf07c5f3da736937ac2360ddbc6
Jaewan-Lim <42615916+JWdori@users.noreply.github.com>
2023-05-30 22:09:25 +0900

    [update] GameManager 수정

--- Project_NCT/Assets/SMT/Scripts/Game3/Balloon_Pop_Fail.cs
+++ Project_NCT/Assets/SMT/Scripts/Game3/Balloon_Pop_Fail.cs
@@ -3,19 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Balloon_Pop_Fail : MonoBehaviour
-{
+{ 
     public ParticleSystem particleSystem;
+    private GameManager3 gameManager;
+    private bool popped = false; // 충돌 시 한 번만 실행하기 위한 변수
 
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager3>();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        particleSystem.gameObject.SetActive(true);
 
+        if (!popped)
+        {
+            particleSystem.gameObject.SetActive(true);
 
-        ParticleSystem instantiatedParticleSystem = Instantiate(particleSystem, collision.contacts[0].point, Quaternion.identity);
-        instantiatedParticleSystem.Play();
+            ParticleSystem instantiatedParticleSystem = Instantiate(particleSystem, collision.contacts[0].point, Quaternion.identity);
+            instantiatedParticleSystem.Play();
 
-        // 충돌 위치에 파티클 시스템 작동
+            // 충돌 위치에 파티클 시스템 작동
+
+            // 점수 증가
+            GameManager3 gameManager = FindObjectOfType<GameManager3>();
+            gameManager.AddScore(-99);
+            gameManager.PrintScore();
+
+            popped = true; // 한 번 실행되었음을 표시
+        }
+
 
         // 충돌한 오브젝트 파괴
         Destroy(gameObject);
