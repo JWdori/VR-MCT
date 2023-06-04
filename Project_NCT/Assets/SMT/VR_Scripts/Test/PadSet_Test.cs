0f99881784865f876ba71f01d29b626510f09e99
KJH-27 <kjh981227@hanyang.ac.kr>
2023-06-04 20:49:32 +0900

    testmode -ing

---
+++ Project_NCT/Assets/SMT/VR_Scripts/Test/PadSet_Test.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// * : Pad ¼³Á¤ ÈÄ 0.5 Ä­ ¶ç±â
+// . : 0.05 Ä­ ¶ç±â
+// > : xÃà ¹æÇâ 0.5Ä­ ¶ç±â
+// ^ : zÃà ¹æÇâ À§·Î 0.5Ä­ ¿Ã¸®±â
+public class PadSet_Test : MonoBehaviour
+{
+    static public string[][] stage = new string[][]
+    {
+        //Test ¸ğµå Pad ¹è¿­
+        new string[]
+        {
+            "  .*.*.*.*.*.  ",
+            "  .*.*.*.*.*.  ",
+            "  .*.*.*.*.*.  "
+        }
+    };
+}
