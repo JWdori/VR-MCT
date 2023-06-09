9a62ba9003229cca7fe66ef1161c6537dd50a97c
KJH-27 <kjh981227@hanyang.ac.kr>
2023-06-09 17:58:11 +0900

    game3 haptic -ing

--- Project_NCT/Assets/SMT/Scripts/Game2/ButtonCtrlLeft.cs
+++ Project_NCT/Assets/SMT/Scripts/Game2/ButtonCtrlLeft.cs
@@ -1,10 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Bhaptics.SDK2;
 
 public class ButtonCtrlLeft : MonoBehaviour
 {
     public int isbuttonPressed = 0;
+
+    bool isLeft = false;
+    bool isRight = false;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("left_hand"))
+        {
+            Debug.Log("Left Hand");
+            isLeft = true;
+        }
+        else if (collision.gameObject.CompareTag("right_hand"))
+        {
+            Debug.Log("Right Hand");
+            isRight = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("left_hand"))
+        {
+            Debug.Log("Left Hand");
+            isLeft = false;
+        }
+        else if (collision.gameObject.CompareTag("right_hand"))
+        {
+            Debug.Log("Right Hand");
+            isRight = false;
+        }
+    }
 
     static public void TouchButton()
     {
